b125c7ec6e027924159f85a94b5fe52d6abc05e1
AbdelrhmanHassan <96892849+doda54@users.noreply.github.com>
2022-10-22 20:11:17 +0700

    Add files via upload

---
+++ javascript/Main.js
@@ -0,0 +1,53 @@
+let navbar = document.querySelector("nav");
+let scrollbtn = document.querySelector(".scroll");
+let links = document.querySelector("ul");
+let Home = document.querySelector("#Home");
+let about = document.querySelector("#About");
+let Services = document.querySelector("#Services");
+let port = document.querySelector("#PORTFOLIO");
+let Contact = document.querySelector("#Contact");
+
+
+
+  window.onscroll = function () {
+    if (window.scrollY >= 50) {
+      navbar.style.background = "#000";
+     
+    } else if (window.scrollY >= 0) {
+      navbar.style.background = "transparent";
+      
+    } 
+  };
+scrollbtn.onclick = function () {
+  window.scroll({
+    top: 950,
+  });
+};
+Home.onclick = function () {
+  window.scroll({
+    top: 0,
+  });
+};
+about.onclick = function () {
+  window.scroll({
+    top: 930,
+  });
+};
+Services.onclick = function () {
+  window.scroll({
+    top: 1800,
+  });
+};
+port.onclick = function () {
+  window.scroll({
+    top: 2700,
+  });
+};
+Contact.onclick = function () {
+  window.scroll({
+    top: 3100,
+  });
+};
+
+
+// 
